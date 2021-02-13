d8d3df6aa0b71495cc1f23dc9c927441c6e63a89
Jose Miguel Reyes <jacrcucuta@gmail.com>
2021-02-13 09:16:25 -0500

    Inicio del proyecto

---
+++ src/main/java/com/tsbook/springboot/web/app/TsbookApplication.java
@@ -0,0 +1,13 @@
+package com.tsbook.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsbook/springboot/web/app/TsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsbook.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
